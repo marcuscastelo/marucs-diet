6dfe24ca32eda7e521e07dfaf8766e1cb72588af
marcuscastelo <marcuscastelo@usp.br>
2023-06-02 08:24:33 -0300

    Visual tunning

--- app/Meal.tsx
+++ app/Meal.tsx
@@ -8,8 +8,16 @@ export default async function Meal(props: MealData) {
             <div className="bg-gray-600 p-3">
                 <h5 className="text-3xl mb-2">Café da manhã</h5>
                 {
-                    props.items.map((item, _index) => <MealItem key={item.id} {...item} />)
+                    props.items.map((item, _index) =>
+                        <div key={item.id} className="mt-2">
+                            <MealItem {...item} />
+                        </div>
+                    )
                 }
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 min-w-full rounded mt-3">
+                    Adicionar item
+                </button>
+
             </div>
         </>
     )
--- app/MealItem.tsx
+++ app/MealItem.tsx
@@ -4,8 +4,8 @@ import MacroNutrients from "./MacroNutrients";
 export default async function MealItem(props: MealItemData) {
     return (
         <>
-            <a href="#" className="block p-6 bg-gray-800 border border-gray-700 rounded-lg shadow hover:bg-gray-700">
-                <h5 className="mb-2 text-md font-bold tracking-tight text-white">{props.food.name}</h5>
+            <a href="#" className="block p-3 bg-gray-800 border border-gray-700 rounded-lg shadow hover:bg-gray-700">
+                <h5 className="mb-2 text-lg font-bold tracking-tight text-white">{props.food.name}</h5>
 
                 <div className="flex">
                     <MacroNutrients {...props.food.macros} />
