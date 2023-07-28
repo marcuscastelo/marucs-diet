ae9bd5785cc281a9c7d460f71e5325d855925d75
marcuscastelo <marcuscastelo@usp.br>
2023-07-28 20:58:54 -0300

    Trying to blur new item input

--- app/newItem/[date]/[mealId]/page.tsx
+++ app/newItem/[date]/[mealId]/page.tsx
@@ -91,10 +91,6 @@ export default function Page(context: any) {
         });
     }
 
-    const onUserFavoritesChanged = async (user: User) => {
-        ;
-    }
-
     useEffect(() => {
         setIsClient(true);
     }, []);
@@ -227,7 +223,16 @@ export default function Page(context: any) {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input autoFocus={isDesktop} value={search} onChange={(e) => setSearch(e.target.value)} type="search" id="default-search" className="block w-full p-4 pl-10 text-sm bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar alimentos" required />
+                <input 
+                    autoFocus={isDesktop} 
+                    value={search} 
+                    onChange={(e) => setSearch(e.target.value)} 
+                    type="search" 
+                    id="default-search" 
+                    className="block w-full p-4 pl-10 text-sm bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" 
+                    placeholder="Buscar alimentos" 
+                    required 
+                />
             </div>
 
             {!searchingFoods && !typing && filteredFoods.length == 0 && <Alert color="warning" className="mt-2">Nenhum alimento encontrado para a busca &quot;{search}&quot;.</Alert>}
