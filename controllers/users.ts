85ff243dccd59a72b34ecdb5d3c794349a01637a
marcuscastelo <marcuscastelo@usp.br>
2023-07-29 04:39:37 -0300

    Reenable barcode supabase

--- controllers/users.ts
+++ controllers/users.ts
@@ -5,10 +5,16 @@ const TABLE = 'users';
 
 export const listUsers = async (): Promise<User[]> => ((await supabase.from(TABLE).select('*')).data ?? []).map((user) => userSchema.parse(user));
 
-export const updateUser = async (id: User['id'], user: User): Promise<User[]> => ((await supabase
+export const updateUser = async (id: User['id'], user: User): Promise<User> => {
+    const { data, error } = await supabase
         .from(TABLE)
         .update(user)
         .eq('id', id)
-        .select()).data ?? []).map((user) => userSchema.parse(user));
+        .select('*');
 
+    if (error) {
+        throw error;
+    }
 
+    return userSchema.parse(data?.[0] ?? null);
+}
