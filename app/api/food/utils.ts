5456370b45b2f09ddbe4362dcfd5f031fb42b858
marcuscastelo <marcuscastelo@usp.br>
2023-07-29 10:37:20 -0300

    Remove logging from API

--- app/api/food/utils.ts
+++ app/api/food/utils.ts
@@ -1,10 +1,6 @@
 import FOOD_SECRETS from '@/secrets/food_api.json';
 
 import axios from 'axios';
-import fs from 'fs/promises';
-
-let calls = 0;
-const RANDOM_ID = Math.floor(Math.random() * 10000000).toString();
 
 export const searchFoodNameInternal = async (food: string) => {
     const url = `${FOOD_SECRETS.base_url}/${FOOD_SECRETS.food_endpoint}`;
@@ -17,10 +13,6 @@ export const searchFoodNameInternal = async (food: string) => {
     });
     console.log(response.data);
     console.dir(response.data);
-
-    calls++;
-    await fs.mkdir(`./logs/${RANDOM_ID}`, { recursive: true });
-    await fs.writeFile(`./logs/${RANDOM_ID}/response-${calls}.json`, JSON.stringify(response.data, null, 2));
 
     return response.data;
 }
