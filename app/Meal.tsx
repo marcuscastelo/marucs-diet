228f45967e6ded9fd4dc8e0ea472196df2bd4569
marcuscastelo <marcuscastelo@usp.br>
2023-07-28 21:21:14 -0300

    Add trash icon + animations

--- app/Meal.tsx
+++ app/Meal.tsx
@@ -52,8 +52,8 @@ export default function Meal({ mealData, onNewItem, onEditItem, onUpdateMeal, cl
         onUpdateMeal(newMealData);
     }
 
-    const onClearItems = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
+    const onClearItems = (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e?.preventDefault();
 
         if (locked) {
             alert('Não é possível limpar itens de uma refeição bloqueada, desbloqueie-a primeiro.');
@@ -110,12 +110,12 @@ export default function Meal({ mealData, onNewItem, onEditItem, onUpdateMeal, cl
                                     &nbsp;
                                     {locked && <span className="text-red-700">[TRAVADA]</span>}
                                 </h5>
-                                <button
-                                    className={`btn ${locked ? 'bg-gray-500' : 'bg-red-800'} px-5 ml-auto text-white`}
-                                    onClick={onClearItems}
+                                <div
+                                    className={`${locked ? 'text-gray-500' : 'text-red-600'} px-5 ml-auto mt-1 text-white btn btn-ghost hover:scale-105`}
+                                    onClick={_ => onClearItems()}
                                 >
-                                    Limpar itens
-                                </button>
+                                    <TrashIcon/>
+                                </div>
                             </div>
                             {
                                 mealData.items.map((item, index) =>
@@ -153,3 +153,5 @@ export default function Meal({ mealData, onNewItem, onEditItem, onUpdateMeal, cl
         </DragDropContext>
     )
 }
+
+const TrashIcon = () => <svg className="bi bi-trash" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" fill="red"></path> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" fill="red"></path> </svg>
