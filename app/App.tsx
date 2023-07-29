3ed7a0fd13eaab4e427a23bdb9a3a36c6bff8098
marcuscastelo <marcuscastelo@usp.br>
2023-07-29 01:28:29 -0300

    Test retrieve users via supabase

--- app/App.tsx
+++ app/App.tsx
@@ -6,21 +6,21 @@ import { listUsers, updateUser } from '@/controllers/users';
 import { User } from '@/model/userModel';
 
 export default function App({ children }: { children: React.ReactNode }) {
-    const dispatch = useAppDispatch();
-    const onChangeUser = (user: User) => dispatch(setUserJson(JSON.stringify(user)));
-    listUsers().then(users => {
-        const localStoredUserId = localStorage.getItem('user');
-        if (localStoredUserId) {
-            const user = users.find(user => user.id === localStoredUserId);
-            if (user) {
-                onChangeUser(user)
-            } else if (users.length > 0) {
-                onChangeUser(users[0])
-            }
-        } else if (users.length > 0) {
-            onChangeUser(users[0])
-        }
-    })
+    // const dispatch = useAppDispatch();
+    // const onChangeUser = (user: User) => dispatch(setUserJson(JSON.stringify(user)));
+    // listUsers().then(users => {
+    //     const localStoredUserId = localStorage.getItem('user');
+    //     if (localStoredUserId) {
+    //         const user = users.find(user => user.id === localStoredUserId);
+    //         if (user) {
+    //             onChangeUser(user)
+    //         } else if (users.length > 0) {
+    //             onChangeUser(users[0])
+    //         }
+    //     } else if (users.length > 0) {
+    //         onChangeUser(users[0])
+    //     }
+    // })
 
     return (
         <>{children}</>
---
+++ app/test/supabase/page.tsx
@@ -0,0 +1,24 @@
+import { listUsers } from "@/controllers/users";
+
+
+export default async function Cart() {
+
+  let users = await listUsers();
+
+  return (
+    <div>
+      {users?.map(user => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+        </div>
+      )) ?? 'No users'
+      }
+    </div>
+  );
+}
+
+// OL4rsk9Ztwf6RnlB
+
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiaGh4Z2VhZmx6bXpwbWF0bmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA2MDMwMDEsImV4cCI6MjAwNjE3OTAwMX0.wRQ_utWfnmSEVhtEysiobLmntfM3GLLcToc2rD1MMPc
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiaGh4Z2VhZmx6bXpwbWF0bmlyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MDYwMzAwMSwiZXhwIjoyMDA2MTc5MDAxfQ.DxI2ekjAMZNlCJilpo4D3o8OMyTOY6iXd0NJWT_C4vY
