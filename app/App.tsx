74a5d3a0aae5eb932afafe89561ff6878706722e
marcuscastelo <marcuscastelo@usp.br>
2023-07-29 21:36:40 -0300

    Fix infinite rerender

--- app/App.tsx
+++ app/App.tsx
@@ -1,27 +1,30 @@
 'use client';
 
-import { useAppDispatch } from '@/redux/hooks';
 import { useUser } from '@/redux/features/userSlice';
 import { listUsers } from '@/controllers/users';
 import { User } from '@/model/userModel';
+import { useEffect } from 'react';
 
 export default function App({ children }: { children: React.ReactNode }) {
     //TODO: retriggered: useUser hook (all over the entire app. Search for useAppDispatch, useAppSelector and dispatch)
     const { setUserJson } = useUser();
-    const onChangeUser = (user: User) => setUserJson(JSON.stringify(user));
-    listUsers().then(users => {
-        const localStoredUserId = ((typeof window !== 'undefined') &&  localStorage) ? parseInt(localStorage.getItem('user') ?? '') : null;
-        if (localStoredUserId) {
-            const user = users.find(user => user.id === localStoredUserId);
-            if (user) {
-                onChangeUser(user)
+
+    useEffect(() => {
+        const onChangeUser = (user: User) => setUserJson(JSON.stringify(user));
+        listUsers().then(users => {
+            const localStoredUserId = ((typeof window !== 'undefined') && localStorage) ? parseInt(localStorage.getItem('user') ?? '') : null;
+            if (localStoredUserId) {
+                const user = users.find(user => user.id === localStoredUserId);
+                if (user) {
+                    onChangeUser(user)
+                } else if (users.length > 0) {
+                    onChangeUser(users[0])
+                }
             } else if (users.length > 0) {
                 onChangeUser(users[0])
             }
-        } else if (users.length > 0) {
-            onChangeUser(users[0])
-        }
-    })
+        })
+    }, [setUserJson]);
 
     return (
         <>{children}</>
