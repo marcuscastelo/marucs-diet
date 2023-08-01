22a747a8bed18213f26829c86f28dabf5a6a6d4e
marcuscastelo <marcuscastelo@usp.br>
2023-07-31 22:51:23 -0300

    Fix intermittent deploy problem

--- app/test/supabase/page.tsx
+++ app/test/supabase/page.tsx
@@ -1,21 +1,52 @@
+'use client'
+
 import { listFoods } from '@/controllers/food'
 import { listUsers } from '@/controllers/users'
+import { Food } from '@/model/foodModel'
+import { User } from '@/model/userModel'
+import { Loadable } from '@/utils/loadable'
+import { useEffect, useState } from 'react'
 
-export default async function Cart() {
-  const users = await listUsers()
-  const foods = await listFoods()
+export default function Cart() {
+  const [users, setUsers] = useState<Loadable<User[]>>({
+    loading: true,
+  })
+
+  const [foods, setFoods] = useState<Loadable<Food[]>>({
+    loading: true,
+  })
+
+  useEffect(() => {
+    listUsers().then((users) => {
+      setUsers({
+        loading: false,
+        data: users,
+      })
+    })
+
+    listFoods().then((foods) => {
+      setFoods({
+        loading: false,
+        data: foods,
+      })
+    })
+  }, [])
+
+  if (users.loading || foods.loading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>
       <h1 className="mb-3 pt-5 text-3xl font-bold">Users</h1>
-      {users?.map((user) => (
+      {users.data.map((user) => (
         <div key={user.id}>
           <h3>{user.name}</h3>
         </div>
       )) ?? 'No users'}
 
       <h1 className="mb-3 pt-5 text-3xl font-bold">Foods</h1>
-      {foods?.map((food) => (
+      {foods.data.map((food) => (
         <div key={food.id}>
           <h3>{food.name}</h3>
         </div>
