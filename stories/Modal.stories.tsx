d5762abd88d29eae437b5a417955e033828d41e4
marcuscastelo <marcuscastelo@usp.br>
2023-08-06 22:47:53 -0300

    Finish storybook installation

---
+++ stories/Modal.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Modal, {
+  ModalActions,
+  ModalBody,
+  ModalHeader,
+} from '../app/(modals)/modal'
+
+const meta: Meta<typeof Modal> = {
+  component: Modal,
+}
+
+export default meta
+type Story = StoryObj<typeof Modal>
+
+export const Root: Story = {
+  render: () => {
+    return (
+      <Modal
+        modalId="teste123"
+        show={true}
+        header={
+          <>
+            <h1>Header</h1>
+          </>
+        }
+        body={
+          <>
+            <h1>Body</h1>
+          </>
+        }
+        actions={
+          <>
+            <h1>Actions</h1>
+          </>
+        }
+      />
+    )
+  },
+}
+
+export const Full: Story = {
+  render: () => {
+    return (
+      <Modal
+        modalId="teste123"
+        show={true}
+        header={<ModalHeader />}
+        body={<ModalBody />}
+        actions={
+          <ModalActions>
+            <div className="btn">Action 1</div>
+            <div className="btn">Action 2</div>
+          </ModalActions>
+        }
+      />
+    )
+  },
+}
