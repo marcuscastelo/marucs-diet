b5e32f985c7a831ad5fcfe30f29f409fc6cba8ae
marcuscastelo <marcuscastelo@usp.br>
2023-08-06 23:03:36 -0300

    Improve modal story

--- stories/Modal.stories.tsx
+++ stories/Modal.stories.tsx
@@ -14,46 +14,40 @@ export default meta
 type Story = StoryObj<typeof Modal>
 
 export const Root: Story = {
-  render: () => {
-    return (
-      <Modal
-        modalId="teste123"
-        show={true}
-        header={
-          <>
-            <h1>Header</h1>
-          </>
-        }
-        body={
-          <>
-            <h1>Body</h1>
-          </>
-        }
-        actions={
-          <>
-            <h1>Actions</h1>
-          </>
-        }
-      />
-    )
+  args: {
+    modalId: 'teste123',
+    show: true,
+    hasBackdrop: true,
+    header: (
+      <>
+        <h1>Header</h1>
+      </>
+    ),
+    body: (
+      <>
+        <h1>Body</h1>
+      </>
+    ),
+    actions: (
+      <>
+        <h1>Actions</h1>
+      </>
+    ),
   },
 }
 
 export const Full: Story = {
-  render: () => {
-    return (
-      <Modal
-        modalId="teste123"
-        show={true}
-        header={<ModalHeader />}
-        body={<ModalBody />}
-        actions={
-          <ModalActions>
-            <div className="btn">Action 1</div>
-            <div className="btn">Action 2</div>
-          </ModalActions>
-        }
-      />
-    )
+  args: {
+    modalId: 'teste123',
+    show: true,
+    hasBackdrop: true,
+    header: <ModalHeader />,
+    body: <ModalBody />,
+    actions: (
+      <ModalActions>
+        <div className="btn">Action 1</div>
+        <div className="btn">Action 2</div>
+      </ModalActions>
+    ),
   },
 }
