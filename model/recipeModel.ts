f638537d7fa46041d87d41e81e5474d34fc5fd42
marcuscastelo <marcuscastelo@usp.br>
2023-08-06 22:47:42 -0300

    Fix build

--- model/recipeModel.ts
+++ model/recipeModel.ts
@@ -8,7 +8,7 @@ export const recipeSchema = z.object({
   name: z.string(),
   owner: z.number(),
   items: z.array(itemSchema), // TODO: Think of a way to avoid id reuse on each item and bugs
-  prepared_multiplier: z.number().default(1),
+  prepared_multiplier: z.number().default(1), // TODO: Rename all snake_case to camelCase (also in db)
   '': z
     .string()
     .nullable()
@@ -22,15 +22,18 @@ export type Recipe = z.infer<typeof recipeSchema>
 export function createRecipe({
   name,
   items,
+  preparedMultiplier = 1,
 }: {
   name: string
   items: Recipe['items']
+  preparedMultiplier?: number
 }): Recipe {
   return {
     id: Math.floor(Math.random() * 1000000),
     owner: 3, // TODO: Get user id
     name,
     items,
+    prepared_multiplier: preparedMultiplier,
     '': 'Recipe',
   }
 }
