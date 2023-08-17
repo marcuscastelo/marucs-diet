33d2852edba90d6dc9adab1488082efbe96dcfa8
marcuscastelo <marcuscastelo@usp.br>
2023-08-16 21:47:37 -0300

    Replace all confirm() with ConfirmModal

--- app/day/[day]/DeleteDayButton.tsx
+++ app/day/[day]/DeleteDayButton.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useConfirmModalContext } from '@/context/confirmModal.context'
 import { deleteDay } from '@/controllers/days'
 import { Day } from '@/model/dayModel'
 
@@ -10,16 +11,25 @@ export default function DeleteDayButton({
   dayData: Day | null | undefined
   refetchDays: () => void
 }) {
+  const { show: showConfirmModal } = useConfirmModalContext()
+
   return (
     <button
       className="btn-error btn mt-3 min-w-full rounded px-4 py-2 font-bold text-white hover:bg-red-400"
-      onClick={async () => {
-        if (!confirm('Tem certeza que deseja excluir este dia?')) {
-          return
-        }
-        // TODO: Avoid non-null assertion
-        await deleteDay(dayData!.id)
-        refetchDays()
+      onClick={() => {
+        showConfirmModal({
+          title: 'Excluir dia',
+          message: 'Tem certeza que deseja excluir este dia?',
+          onConfirm: async () => {
+            if (dayData) {
+              await deleteDay(dayData.id)
+              refetchDays()
+            } else {
+              console.error('Day data is null')
+              throw new Error('Day data is null')
+            }
+          },
+        })
       }}
     >
       PERIGO: Excluir dia
