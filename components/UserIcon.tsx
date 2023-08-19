8720314f3e7cb822758927820c12ac1c3bf8f72f
marcuscastelo <marcuscastelo@usp.br>
2023-08-19 20:57:31 -0300

    Fix <Image> size error

--- components/UserIcon.tsx
+++ components/UserIcon.tsx
@@ -1,5 +1,14 @@
 import Image from 'next/image'
 
 export default function UserIcon() {
-  return <Image className="h-10 w-10 rounded-full" src="/user.png" alt="" />
+  return (
+    <Image
+      className="h-10 w-10 rounded-full"
+      src="/user.png"
+      sizes="100vw"
+      alt=""
+      width={0}
+      height={0}
+    />
+  )
 }
