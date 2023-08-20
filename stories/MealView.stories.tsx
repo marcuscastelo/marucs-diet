ea9e2b62e9a133b7a9dc213b74e614e95e8f604f
marcuscastelo <marcuscastelo@usp.br>
2023-08-19 21:09:37 -0300

    Rename MealView to MealEditView Fixes #157

--- stories/MealView.stories.tsx
+++ stories/MealView.stories.tsx
@@ -1,28 +1,27 @@
-import MealView from '@/app/(meal)/MealView'
-import App from '@/app/App'
+import MealEditView from '@/app/(meal)/MealEditView'
 import ServerApp from '@/app/ServerApp'
 import { mockMeal } from '@/app/test/unit/(mock)/mockData'
 import { Meta, StoryObj } from '@storybook/react'
 
-const meta: Meta<typeof MealView> = {
-  title: 'Components/MealView',
-  component: MealView,
+const meta: Meta<typeof MealEditView> = {
+  title: 'Components/MealEditView',
+  component: MealEditView,
   decorators: [(Story) => <ServerApp>{Story()}</ServerApp>], // TODO: Create <MockApp> to provide context to all stories
 }
 
 export default meta
-type Story = StoryObj<typeof MealView>
+type Story = StoryObj<typeof MealEditView>
 
 export const Root: Story = {
   args: {
     mealData: mockMeal(), // TODO: display group with 3 (orange), group with 1 (white), and recipe (blue)
   },
   render: (args) => (
-    <MealView
+    <MealEditView
       {...args}
-      header={<MealView.Header onUpdateMeal={() => undefined} />}
-      content={<MealView.Content onEditItemGroup={() => undefined} />}
-      actions={<MealView.Actions onNewItem={() => undefined} />}
+      header={<MealEditView.Header onUpdateMeal={() => undefined} />}
+      content={<MealEditView.Content onEditItemGroup={() => undefined} />}
+      actions={<MealEditView.Actions onNewItem={() => undefined} />}
     />
   ),
 }
