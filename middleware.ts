b34dda43f8f4d152119abea97671db3d5814ac6d
marcuscastelo <marcuscastelo@usp.br>
2023-08-20 13:17:44 -0300

    userId cookie lasts only for session, instead of being permanent Fixes #255

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,11 @@ export function middleware(request: NextRequest) {
   const res = NextResponse.next()
   // If USER_ID cookie is not set, set it to DEFAULT_USER_ID
   if (!request.cookies.has(COOKIES.USER_ID)) {
-    res.cookies.set(COOKIES.USER_ID, COOKIES.DEFAULT_USER_ID)
+    // TODO: After setting cookie, redirect to the same page to make sure the cookie is set when the page loads
+    res.cookies.set(COOKIES.USER_ID, COOKIES.DEFAULT_USER_ID, {
+      expires: new Date('2038-01-19T03:14:07.000Z'),
+      httpOnly: true,
+    })
   }
 
   return res
