2ae52d2231b11acfd905117ffb6cf8ddcd19ec9d
marcuscastelo <marcuscastelo@usp.br>
2023-08-20 11:28:43 -0300

    Delete secrets 3

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+import { z } from 'zod'
 
-const supabaseUrl = '***REMOVED***'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey as string, {
+const supabaseUrl = z.string().parse(process.env.NEXT_PUBLIC_SUPABASE_URL)
+const supabaseKey = z.string().parse(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+const supabase = createClient(supabaseUrl, supabaseKey, {
   db: {
     schema: 'public',
   },
