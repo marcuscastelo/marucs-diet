8d9bbdd30b4f59975af147298839c299e985fe76
marcuscastelo <marcuscastelo@usp.br>
2023-08-25 14:56:14 -0300

    Fix tooltip and average

--- components/chart/CandleStickChart.tsx
+++ components/chart/CandleStickChart.tsx
@@ -8,6 +8,7 @@ import {
   ComposedChart,
   Line,
   ResponsiveContainer,
+  Tooltip,
   XAxis,
   YAxis,
 } from 'recharts'
@@ -35,7 +36,7 @@ const prepareData = <T extends OHLC>(data: T[]) => {
   })
 }
 
-export const CandleStickCharta = <T extends OHLC>({
+export const CandleStickChart = <T extends OHLC>({
   data: rawData,
   children,
 }: {
@@ -52,52 +53,44 @@ export const CandleStickCharta = <T extends OHLC>({
   console.log(minValue, maxValue)
 
   return (
-    <ComposedChart data={data} className="outline">
-      <XAxis dataKey="date" />
-      <YAxis
-        domain={[minValue, maxValue]}
-        type="number"
-        allowDecimals={false}
-        ticks={Array.from(Array(10).keys()).map(
-          (i) => Math.floor(minValue + (maxValue - minValue) * (i / 10)) + 1,
-        )}
-      />
-      <CartesianGrid opacity={0.1} />
-      <Bar
-        dataKey="openClose"
-        fill="#8884d8"
-        shape={CandleStickShape}
-        // label={{ position: 'top' }}
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-        ))}
-      </Bar>
-      {children}
-    </ComposedChart>
-  )
-}
-
-export default function CandleStickChart<T extends OHLC>({
-  data,
-}: {
-  data: T[]
-}) {
-  return (
     <ResponsiveContainer width="95%" height={400}>
-      <ComposedChart data={data} width={0} height={400}>
+      <ComposedChart data={data} className="">
         <XAxis dataKey="date" />
-        <YAxis type="number" allowDecimals={false} />
-        <CartesianGrid opacity={0.1} />
-        <Line
-          type="monotone"
-          dataKey="movingAverage"
-          stroke="orange"
-          fill="orange"
-          dot={false}
-          strokeWidth={3}
-          opacity={0.2}
+        <YAxis
+          domain={[minValue, maxValue]}
+          type="number"
+          allowDecimals={false}
+          ticks={Array.from(Array(10).keys()).map(
+            (i) => Math.floor(minValue + (maxValue - minValue) * (i / 10)) + 1,
+          )}
         />
+        <Tooltip
+          content={({ payload, label, active }) => {
+            if (!active || !payload) return null
+            const { low, high, movingAverage } = payload[0].payload
+            return (
+              <div className="bg-slate-700 p-5 opacity-80">
+                <h1>
+                  Variação: {low} ~ {high}
+                </h1>
+                <h1>Média: {movingAverage}</h1>
+                <h1>{label}</h1>
+              </div>
+            )
+          }}
+        />
+        <CartesianGrid opacity={0.1} />
+        <Bar
+          dataKey="openClose"
+          fill="#8884d8"
+          shape={CandleStickShape}
+          // label={{ position: 'top' }}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+        {children}
       </ComposedChart>
     </ResponsiveContainer>
   )
