6e302a6e07154fbd6dc83fe0759a448eabbd17c1
marcuscastelo <marcuscastelo@usp.br>
2023-08-25 10:38:41 -0300

    Add cookie expiration info to server actions

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { COOKIES } from './constants/cookies'
+import { createUserIdCookie } from './cookies/userId'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const res = NextResponse.next()
+
   // If USER_ID cookie is not set, set it to DEFAULT_USER_ID
   if (!request.cookies.has(COOKIES.USER_ID)) {
-    // TODO: After setting cookie, redirect to the same page to make sure the cookie is set when the page loads
-    res.cookies.set(COOKIES.USER_ID, COOKIES.DEFAULT_USER_ID, {
-      expires: new Date('2024-01-19T03:14:07.000Z'),
-      httpOnly: true,
-    })
+    const { key, value, cookie } = createUserIdCookie(COOKIES.DEFAULT_USER_ID)
+    res.cookies.set(key, value, cookie)
   }
 
   return res
