e6f767cceaf3dae2d0d11e71c24f129794757013
marcuscastelo <marcuscastelo@usp.br>
2023-09-14 08:20:11 -0300

    Add support for profile pictures Fixes #38

--- components/UserIcon.tsx
+++ components/UserIcon.tsx
@@ -1,10 +1,18 @@
+import { getUser } from '@/actions/user'
+import { fetchUser } from '@/controllers/users'
 import Image from 'next/image'
 
-export default function UserIcon() {
+export default async function UserIcon() {
+  const userId = await getUser()
+
+  if (!userId) {
+    return <h1>Usuário {userId} não definido</h1>
+  }
+
   return (
     <Image
       className="h-10 w-10 rounded-full"
-      src="/user.png"
+      src={`https://sbhhxgeaflzmzpmatnir.supabase.co/storage/v1/object/public/uploads/${userId}.jpg`}
       sizes="100vw"
       alt=""
       width={0}
