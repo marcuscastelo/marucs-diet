d14f42357c12ebf237151b8674b09fb79277b795
marcuscastelo <marcuscastelo@usp.br>
2023-09-18 11:58:07 -0300

    Fix build

--- components/ConfirmModal.tsx
+++ components/ConfirmModal.tsx
@@ -5,7 +5,7 @@ import { useConfirmModalContext } from '@/context/confirmModal.context'
 export default function ConfirmModal() {
   const {
     visible,
-    internals: { title, body: body, setVisible, actions },
+    internals: { title, body, setVisible, actions },
   } = useConfirmModalContext()
 
   return (
