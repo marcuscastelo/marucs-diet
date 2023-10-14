32d922cbb3ef1c37ea75e988d0ed9e44437e3ae4
marcuscastelo <marcuscastelo@usp.br>
2023-10-13 23:21:54 -0300

    [#448] Delete all "unit test pages"

--- src/app/test/unit/fooditem/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import FoodItemView from '@/app/(foodItem)/FoodItemView'
-import { mockItem } from '@/app/test/unit/(mock)/mockData'
-import { computed } from '@preact/signals-react'
-
-export default function FoodItemPage() {
-  return <FoodItemView foodItem={computed(mockItem)} />
-}
