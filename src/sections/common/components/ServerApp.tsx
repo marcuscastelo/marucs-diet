5a0b2ffca30d09af4aa11e97a2e1260633aba491
marcuscastelo <marcuscastelo@usp.br>
2023-10-29 18:41:21 -0300

    [#483] Cherry-picking: 4 (App & ServerApp)

--- src/sections/common/components/ServerApp.tsx
+++ src/sections/common/components/ServerApp.tsx
@@ -1,7 +1,4 @@
-import { fetchUser } from '@/legacy/controllers/users'
 import App from '@/sections/common/components/App'
-import { getUser } from '@/legacy/actions/user'
-import { revalidatePath } from 'next/cache'
 
 export default async function ServerApp({
   children,
@@ -10,27 +7,5 @@ export default async function ServerApp({
 }) {
   console.debug(`[ServerApp] - Rendering`)
 
-  const userId = await getUser()
-
-  if (!userId) {
-    return <h1>Usuário não definido</h1>
-  }
-
-  const user = await fetchUser(userId)
-
-  if (!user) {
-    return <h1>Usuário {userId} não encontrado</h1>
-  }
-
-  return (
-    <App
-      user={user}
-      onSaveUser={async () => {
-        'use server'
-        revalidatePath('/')
-      }}
-    >
-      {children}
-    </App>
-  )
+  return <App>{children}</App>
 }
