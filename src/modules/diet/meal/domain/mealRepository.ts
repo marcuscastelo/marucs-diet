ca6f4279318091b17c28386f2a7e20b7a74e5c34
marcuscastelo <marcuscastelo@usp.br>
2023-10-30 04:17:54 -0300

    [#483] Fix build

--- src/modules/diet/meal/domain/mealRepository.ts
+++ src/modules/diet/meal/domain/mealRepository.ts
@@ -1,15 +1,15 @@
-import { DayDiet } from '@/modules/diet/day-diet/domain/dayDiet'
-import { DbReady } from '@/legacy/utils/newDbRecord'
-import { Meal } from '@/modules/diet/meal/domain/meal'
+// import { type DayDiet } from '@/modules/diet/day-diet/domain/dayDiet'
+// import { DbReady } from '@/legacy/utils/newDbRecord'
+// import { type Meal } from '@/modules/diet/meal/domain/meal'
 
-// TODO: Make meal not a subcollection of day
-export interface MealRepository {
-  fetchDayMeals(dayId: DayDiet['id']): Promise<readonly Meal[]>
-  insertMeal(dayId: DayDiet['id'], newMeal: Meal): Promise<Meal | undefined>
-  updateMeal(
-    dayId: DayDiet['id'],
-    mealId: Meal['id'],
-    newMeal: Meal,
-  ): Promise<Meal | undefined>
-  deleteMeal(dayId: DayDiet['id'], id: DayDiet['id']): Promise<void>
-}
+// // TODO: Make meal not a subcollection of day
+// export interface MealRepository {
+//   fetchDayMeals: (dayId: DayDiet['id']) => Promise<readonly Meal[]>
+//   insertMeal: (dayId: DayDiet['id'], newMeal: Meal) => Promise<Meal | undefined>
+//   updateMeal: (
+//     dayId: DayDiet['id'],
+//     mealId: Meal['id'],
+//     newMeal: Meal,
+//   ) => Promise<Meal | undefined>
+//   deleteMeal: (dayId: DayDiet['id'], id: DayDiet['id']) => Promise<void>
+// }
--- src/modules/weight/application/weight.ts
+++ src/modules/weight/application/weight.ts
@@ -1,38 +1,37 @@
-import { DbReady } from '@/legacy/utils/newDbRecord'
-import { Weight } from '@/modules/weight/domain/weight'
-import { createSupabaseWeightRepository } from '@/modules/weight/infrastructure/supabaseWeightRepository'
-import { computed, signal } from '@preact/signals-core'
+// import { type DbReady } from '@/legacy/utils/newDbRecord'
+// import { type Weight } from '@/modules/weight/domain/weight'
+// import { createSupabaseWeightRepository } from '@/modules/weight/infrastructure/supabaseWeightRepository'
 
-const weightRepository = createSupabaseWeightRepository()
+// const weightRepository = createSupabaseWeightRepository()
 
-const userWeights_ = signal<readonly Weight[]>([])
-export const userWeights = computed(() => userWeights_.value)
+// const userWeights_ = signal<readonly Weight[]>([])
+// export const userWeights = computed(() => userWeights_.value)
 
-export async function fetchUserWeights(userId: number) {
-  const weights = await weightRepository.fetchUserWeights(userId)
-  userWeights_.value = weights
-  return weights
-}
+// export async function fetchUserWeights (userId: number) {
+//   const weights = await weightRepository.fetchUserWeights(userId)
+//   userWeights_.value = weights
+//   return weights
+// }
 
-export async function insertWeight(newWeight: DbReady<Weight>) {
-  const weight = await weightRepository.insertWeight(newWeight)
-  if (weight.owner === userWeights_.value[0].owner) {
-    fetchUserWeights(weight.owner)
-  }
-  return weight
-}
+// export async function insertWeight (newWeight: DbReady<Weight>) {
+//   const weight = await weightRepository.insertWeight(newWeight)
+//   if (weight.owner === userWeights_.value[0].owner) {
+//     fetchUserWeights(weight.owner)
+//   }
+//   return weight
+// }
 
-export async function updateWeight(weightId: Weight['id'], newWeight: Weight) {
-  const weight = await weightRepository.updateWeight(weightId, newWeight)
-  if (weight.owner === userWeights_.value[0].owner) {
-    fetchUserWeights(weight.owner)
-  }
-  return weight
-}
+// export async function updateWeight (weightId: Weight['id'], newWeight: Weight) {
+//   const weight = await weightRepository.updateWeight(weightId, newWeight)
+//   if (weight.owner === userWeights_.value[0].owner) {
+//     fetchUserWeights(weight.owner)
+//   }
+//   return weight
+// }
 
-export async function deleteWeight(weightId: Weight['id']) {
-  await weightRepository.deleteWeight(weightId)
-  if (userWeights_.value.length > 0) {
-    fetchUserWeights(userWeights_.value[0].owner)
-  }
-}
+// export async function deleteWeight (weightId: Weight['id']) {
+//   await weightRepository.deleteWeight(weightId)
+//   if (userWeights_.value.length > 0) {
+//     fetchUserWeights(userWeights_.value[0].owner)
+//   }
+// }
