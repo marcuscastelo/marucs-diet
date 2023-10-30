29e47d6aad7595b281f493ecc3012632584b70bb
marcuscastelo <marcuscastelo@usp.br>
2023-10-30 20:47:56 -0300

    [#483] Fix env issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import solid from 'vite-plugin-solid'
 import tsconfigPaths from 'vite-tsconfig-paths'
 // @ts-expect-error eslint is not typed
 import eslint from 'vite-plugin-eslint'
 
-export default defineConfig({
-  plugins: [solid(), eslint(), tsconfigPaths()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [solid(), eslint(), tsconfigPaths()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV)
+    }
+  }
 })
