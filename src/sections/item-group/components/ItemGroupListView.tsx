4f6a15acc0b170388429fb9ca81efb4a0e8dc2a8
marcuscastelo <marcuscastelo@usp.br>
2023-10-31 23:12:02 -0300

    [#483] Add ItemGroupListView

---
+++ src/sections/item-group/components/ItemGroupListView.tsx
@@ -0,0 +1,48 @@
+import { type ItemGroup } from '@/modules/diet/item-group/domain/itemGroup'
+import ItemGroupView, {
+  ItemGroupCopyButton,
+  ItemGroupHeader,
+  ItemGroupName,
+  ItemGroupViewNutritionalInfo,
+  type ItemGroupViewProps
+} from '@/sections/item-group/components/ItemGroupView'
+import { type Accessor } from 'solid-js'
+
+export default function ItemGroupListView (props: {
+  itemGroups: Accessor<ItemGroup[]>
+  onItemClick: ItemGroupViewProps['onClick']
+}) {
+  console.debug('[ItemGroupListView] - Rendering')
+  return (
+    <>
+      {props.itemGroups().map((_, idx) => {
+        const group = () => props.itemGroups()[idx]
+        return (
+          <div class="mt-2">
+            <ItemGroupView
+              itemGroup={group}
+              onClick={props.onItemClick}
+              header={
+                <ItemGroupHeader
+                  name={<ItemGroupName group={group} />}
+                  copyButton={
+                    <ItemGroupCopyButton
+                      group={group}
+                      onCopyItemGroup={(group) => {
+                        // TODO: Replace with clipboard hook (here and in FoodItemView, if applicable)
+                        navigator.clipboard.writeText(JSON.stringify(group)).catch((err) => {
+                          console.error('Failed to copy item group to clipboard', err)
+                        })
+                      }}
+                    />
+                  }
+                />
+              }
+              nutritionalInfo={<ItemGroupViewNutritionalInfo group={group} />}
+            />
+          </div>
+        )
+      })}
+    </>
+  )
+}
