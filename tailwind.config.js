bc40b4dcc304628d92dc5496c99f673e5ff158ae
marcuscastelo <marcuscastelo@usp.br>
2023-11-01 02:06:01 -0300

    [#483] Bringing back some files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './node_modules/flowbite/**/*.js', // configure the Flowbite JS source template paths
     './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        '3xs': '200px',
+        '2xs': '300px',
+        xs: '475px',
+        ...defaultTheme.screens
+      },
+      extend: {
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+        }
+      }
+    }
   },
-  darkMode: 'class',
   plugins: [
-    require('daisyui'),
     require('flowbite/plugin'),
+    require('daisyui'),
     require('tw-elements/dist/plugin.cjs')
   ]
 }
