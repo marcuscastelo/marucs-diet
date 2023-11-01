e4d6ba4896232c21b5ffd5855d392041c4827095
marcuscastelo <marcuscastelo@usp.br>
2023-10-31 21:10:09 -0300

    [#483] Add Datepicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}'
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/flowbite/**/*.js', // configure the Flowbite JS source template paths
+    './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
     extend: {}
   },
-  plugins: [require('daisyui')]
+  darkMode: 'class',
+  plugins: [
+    require('daisyui'),
+    require('flowbite/plugin'),
+    require('tw-elements/dist/plugin.cjs')
+  ]
 }
