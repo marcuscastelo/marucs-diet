cb6e726042dd2693174c8607f38beb991220c353
marcuscastelo <marcuscastelo@usp.br>
2023-11-02 01:38:37 -0300

    [#483] Add MealEditViewList

---
+++ src/sections/meal/components/MealEditViewList.tsx
@@ -0,0 +1,27 @@
+import MealEditView, {
+  type MealEditViewProps
+} from '@/sections/meal/components/MealEditView'
+import { For, type Accessor } from 'solid-js'
+
+export type DayMealsProps = {
+  mealEditPropsList: Accessor<MealEditViewProps[]>
+  className?: string
+}
+
+export default function MealEditViewList (props: DayMealsProps) {
+  return (
+    <div class={props.className}>
+      <For each={props.mealEditPropsList()}>
+        {(_, index) => {
+          const mealPropsSignal = () => props.mealEditPropsList()[index()]
+          return (
+            <MealEditView
+              {...mealPropsSignal()}
+              class="mt-2"
+            />
+          )
+        }}
+      </For>
+    </div>
+  )
+}
