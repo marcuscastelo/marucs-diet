726427b3b71cba3e16457549c93d3b7c20aa747a
marcuscastelo <marcuscastelo@usp.br>
2023-11-02 22:32:22 -0300

    [#483] Add CreateBlankDayButton

--- src/sections/day-diet/components/CopyLastDayButton.tsx
+++ src/sections/day-diet/components/CopyLastDayButton.tsx
@@ -19,57 +19,57 @@ export default function CopyLastDayButton (props: {
   )
   return (
     <>
-    <Show when={lastDay() === undefined}>
-            <button
-        class="btn-error btn mt-3 min-w-full rounded px-4 py-2 font-bold text-white"
-        onClick={
-          () => { alert(`Não foi possível encontrar um dia anterior a ${props.selectedDay}`) } // TODO: Change all alerts with ConfirmModal
-        }
-      >
-        Copiar dia anterior: não encontrado!
-      </button>
-    </Show>
+      <Show when={lastDay() === undefined}>
+        <button
+          class="btn-error btn mt-3 min-w-full rounded px-4 py-2 font-bold text-white"
+          onClick={
+            () => { alert(`Não foi possível encontrar um dia anterior a ${props.selectedDay}`) } // TODO: Change all alerts with ConfirmModal
+          }
+        >
+          Copiar dia anterior: não encontrado!
+        </button>
+      </Show>
 
-    <Show when={lastDay()}>
-      {(lastDay) => (
-    <button
-      class="btn-primary btn mt-3 min-w-full rounded px-4 py-2 font-bold text-white"
-      onClick={() => {
-        const day_ = props.day()
-        if (day_ !== undefined) {
-          showConfirmModal({
-            title: 'Sobrescrever dia',
-            body: 'Tem certeza que deseja SOBRESCREVER este dia?',
-            actions: [
-              {
-                text: 'Cancelar',
-                onClick: () => undefined
-              },
-              {
-                text: 'Sobrescrever',
-                primary: true,
-                onClick: () => {
-                  updateDayDiet(day_.id, {
-                    ...lastDay(),
-                    target_day: props.selectedDay
-                  })
-                }
+      <Show when={lastDay()}>
+        {(lastDay) => (
+          <button
+            class="btn-primary btn mt-3 min-w-full rounded px-4 py-2 font-bold text-white"
+            onClick={() => {
+              const day_ = props.day()
+              if (day_ !== undefined) {
+                showConfirmModal({
+                  title: 'Sobrescrever dia',
+                  body: 'Tem certeza que deseja SOBRESCREVER este dia?',
+                  actions: [
+                    {
+                      text: 'Cancelar',
+                      onClick: () => undefined
+                    },
+                    {
+                      text: 'Sobrescrever',
+                      primary: true,
+                      onClick: () => {
+                        updateDayDiet(day_.id, {
+                          ...lastDay(),
+                          target_day: props.selectedDay
+                        })
+                      }
+                    }
+                  ]
+                })
+                return
               }
-            ]
-          })
-          return
-        }
 
-        insertDayDiet({
-          ...lastDay(),
-          target_day: props.selectedDay
-        })
-      }}
-    >
-      {/* //TODO: Allow copying any past day, not just latest one */}
-      Copiar dia anterior ({lastDay().target_day})
-    </button>
-      )}
+              insertDayDiet({
+                ...lastDay(),
+                target_day: props.selectedDay
+              })
+            }}
+          >
+            {/* //TODO: Allow copying any past day, not just latest one */}
+            Copiar dia anterior ({lastDay().target_day})
+          </button>
+        )}
       </Show>
     </>
   )
