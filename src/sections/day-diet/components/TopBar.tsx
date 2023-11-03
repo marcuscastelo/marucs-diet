549f0b0c289902d78d47f0fa323b4dfad89c86a5
marcuscastelo <marcuscastelo@usp.br>
2023-11-02 22:46:12 -0300

    [#483] Add TopBar

---
+++ src/sections/day-diet/components/TopBar.tsx
@@ -0,0 +1,18 @@
+import { createEffect } from 'solid-js'
+
+// TODO: make day/TopBar a common component
+export default function TopBar (props: { selectedDay: string }) {
+  // TODO: Add datepicker to topbar
+  createEffect(() => {
+    console.debug('TopBar', props.selectedDay)
+  })
+  return (
+    <>
+      <div class="flex items-center justify-between gap-4 bg-slate-900 px-4 py-2">
+        <div class="flex-1">
+          {/* <DatepickerRouter selectedDay={props.selectedDay} /> */}
+        </div>
+      </div>
+    </>
+  )
+}
