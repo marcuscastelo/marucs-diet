7b061c64233e9693218b3c2f6a6b47d6f430f40c
marcuscastelo <marcuscastelo@usp.br>
2023-11-04 16:30:22 -0300

    [#483] Create TargetDayPicker

--- src/sections/day-diet/components/TopBar.tsx
+++ src/sections/day-diet/components/TopBar.tsx
@@ -1,3 +1,4 @@
+import { TargetDayPicker } from '@/sections/common/components/TargetDayPicker'
 import { createEffect } from 'solid-js'
 
 // TODO: make day/TopBar a common component
@@ -10,7 +11,7 @@ export default function TopBar (props: { selectedDay: string }) {
     <>
       <div class="flex items-center justify-between gap-4 bg-slate-900 px-4 py-2">
         <div class="flex-1">
-          {/* <DatePicker class='w-full'/> */}
+          <TargetDayPicker/>
         </div>
       </div>
     </>
