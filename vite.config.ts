3b00d0d95d7a4ec916b92b6cd197ab31b7bdc7f2
marcuscastelo <marcuscastelo@usp.br>
2024-05-11 22:26:06 -0300

    [#527] Remove unused vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
-import solid from 'solid-start/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 // @ts-expect-error eslint is not typed
 import eslint from 'vite-plugin-eslint'
-import devtools from 'solid-devtools/vite'
-import vercel from 'solid-start-vercel'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -13,23 +10,7 @@ export default defineConfig(({ mode }) => {
     optimizeDeps: {
       disabled: true,
     },
-    plugins: [
-      devtools({
-        autoname: true, // e.g. enable autoname
-        locator: {
-          targetIDE: 'vscode-insiders',
-          componentLocation: true,
-          jsxLocation: true,
-          key: 'Alt',
-        },
-      }),
-      solid({
-        ssr: false,
-        adapter: vercel(),
-      }),
-      eslint(),
-      tsconfigPaths(),
-    ],
+    plugins: [eslint(), tsconfigPaths()],
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
