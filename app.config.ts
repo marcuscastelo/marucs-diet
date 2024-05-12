22eaebb800f32e7bbf4cc0b9beb10d75ca644355
marcuscastelo <marcuscastelo@usp.br>
2024-05-12 12:38:07 -0300

    [Feature] Show version on right side

--- app.config.ts
+++ app.config.ts
@@ -7,5 +7,16 @@ export default defineConfig({
   },
   vite: {
     plugins: [],
+    define: {
+      'process.env.APP_VERSION': JSON.stringify(
+        (() => {
+          console.log(
+            'process.env.npm_package_version',
+            process.env.npm_package_version,
+          )
+          return process.env.npm_package_version
+        })(),
+      ),
+    },
   },
 })
