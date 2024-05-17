2b8faa250da38da1c82a05492e6576f52bd17b0e
marcuscastelo <marcuscastelo@usp.br>
2024-05-17 17:17:14 -0300

    [Improvement] Semver generation for RCs

--- .scripts/semver.sh
+++ .scripts/semver.sh
@@ -2,7 +2,6 @@
 
 git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/' | grep -E "rc/" >/dev/null
 isrc=$?
-echo "RC: $isrc"
 if [ $isrc -eq 0 ]; then
   echo "RC logic"
   count=$(git rev-list --count HEAD ^main)
