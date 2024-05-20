901c093996023b8a31078314e942c4f0795e1cc5
marcuscastelo <marcuscastelo@usp.br>
2024-05-20 13:52:42 -0300

    [#556] Create simple settings page

---
+++ src/sections/settings/components/Toggle.tsx
@@ -0,0 +1,30 @@
+import { type JSXElement, type Accessor } from 'solid-js'
+
+export function Toggle(props: {
+  label: JSXElement
+  checked: Accessor<boolean>
+  setChecked: (value: boolean) => void
+  disabled?: boolean
+}) {
+  return (
+    <>
+      <label class="flex items-center cursor-pointer justify-between">
+        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+          {props.label}
+        </span>
+        <input
+          type="checkbox"
+          value=""
+          class="sr-only peer"
+          checked={props.checked()}
+          disabled={props.disabled}
+          onChange={(e) => {
+            e.currentTarget.blur()
+            props.setChecked(e.currentTarget.checked)
+          }}
+        />
+        <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
+      </label>
+    </>
+  )
+}
