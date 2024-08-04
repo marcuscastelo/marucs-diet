9e87f3be6580532d693e40bc4fb1a7a9150b5ace
marcuscastelo <marcuscastelo@usp.br>
2024-08-04 19:08:50 -0300

    refactor: move <Providers> to app

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,21 @@
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
+import { Router } from '@solidjs/router'
+import { FileRoutes } from '@solidjs/start/router'
+import { Suspense } from 'solid-js'
 import '~/assets/styles/globals.css'
+import { Providers } from './sections/common/context/Providers'
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
-          <Suspense>{props.children}</Suspense>
+          <Suspense>
+            <Providers>{props.children}</Providers>
+          </Suspense>
         </>
       )}
     >
-        <FileRoutes />
+      <FileRoutes />
     </Router>
-  );
+  )
 }
