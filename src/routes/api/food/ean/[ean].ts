48866d9dcdd8a9f9aa4f422505826d9e8fd2932c
marcuscastelo <marcuscastelo@usp.br>
2025-06-02 18:17:48 -0300

    fix(api): correct error message formatting for fetching food by EAN

--- src/routes/api/food/ean/[ean].ts
+++ src/routes/api/food/ean/[ean].ts
@@ -21,7 +21,7 @@ export async function GET({ params }: APIEvent) {
     })
     return json(
       {
-        error: 'Error fetching foods by ean: ' + (error as AxiosError).message,
+        error: 'Error fetching food by EAN: ' + (error as AxiosError).message,
       },
       { status: (error as AxiosError).status },
     )
