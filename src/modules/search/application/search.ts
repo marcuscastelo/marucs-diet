066fb6b953e46c58528bd68602a1cc55386baa31
marcuscastelo <marcuscastelo@usp.br>
2025-06-03 17:35:25 -0300

    style(search): format import statements for better readability

--- src/modules/search/application/search.ts
+++ src/modules/search/application/search.ts
@@ -1,5 +1,8 @@
 import { createSignal } from 'solid-js'
-import { type AvailableTab, availableTabs } from '~/sections/search/components/TemplateSearchTabs'
+import {
+  type AvailableTab,
+  availableTabs,
+} from '~/sections/search/components/TemplateSearchTabs'
 
 export const [templateSearch, setTemplateSearch] = createSignal<string>('')
 export const [templateSearchTab, setTemplateSearchTab] =
