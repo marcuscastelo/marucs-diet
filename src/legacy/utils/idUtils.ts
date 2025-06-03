82d78b7570eb1479e751cfe25d75b7d1e91b687e
marcuscastelo <marcuscastelo@usp.br>
2025-06-03 20:02:00 -0300

    refactor(idUtils): remove deprecated addId function and clean up code

--- src/legacy/utils/idUtils.ts
+++ src/legacy/utils/idUtils.ts
@@ -14,12 +14,3 @@ export function generateId(): number {
 export function regenerateId<T extends Identifiable>(obj: T): T {
   return { ...obj, id: generateId() }
 }
-
-/**
- * @deprecated Probably should be done by database
- */
-export function addId<T extends Exclude<object, 'id'>>(
-  obj: T,
-): T & Identifiable {
-  return { ...obj, id: generateId() }
-}
