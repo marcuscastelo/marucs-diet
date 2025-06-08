89ff7175c9b59cd38333eac207019dbc62d11749
marcuscastelo <marcuscastelo@usp.br>
2025-06-07 22:46:34 -0300

    refactor(app.config): replace npm package version retrieval with execSync to fetch version from semver.sh

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from '@solidjs/start/config'
 import tailwindcss from '@tailwindcss/vite'
+import { execSync } from 'child_process'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   ssr: false,
@@ -8,12 +11,15 @@ export default defineConfig({
     define: {
       'process.env.APP_VERSION': JSON.stringify(
         (() => {
-          console.log(
-            'process.env.npm_package_version',
-
-            process.env.npm_package_version,
-          )
-          return process.env.npm_package_version
+          try {
+            const __dirname = dirname(fileURLToPath(import.meta.url))
+            return execSync(resolve(__dirname, '.scripts/semver.sh'), {
+              encoding: 'utf8',
+            }).trim()
+          } catch (e) {
+            console.error('Failed to get version from .scripts/semver.sh', e)
+            return 'unknown'
+          }
         })(),
       ),
     },
