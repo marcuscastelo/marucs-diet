4e35906966ca192b15b85f5089506904f989bf2e
marcuscastelo <marcuscastelo@usp.br>
2025-06-07 22:42:43 -0300

    refactor(config): move APP_VERSION definition from vite.config.ts to app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,18 @@ export default defineConfig({
   ssr: false,
   vite: {
     plugins: [tailwindcss()],
+    define: {
+      'process.env.APP_VERSION': JSON.stringify(
+        (() => {
+          console.log(
+            'process.env.npm_package_version',
+
+            process.env.npm_package_version,
+          )
+          return process.env.npm_package_version
+        })(),
+      ),
+    },
   },
   server: {
     preset: 'vercel',
