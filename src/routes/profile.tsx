2aa4f34463777461c41539cbd2f8b00a81c3b0ad
marcuscastelo <marcuscastelo@usp.br>
2025-06-09 05:36:19 -0300

    feat(profile): adapt width dynamically to viewport height for 9:16 aspect

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,5 @@
+import { createSignal, onCleanup } from 'solid-js'
+
 import { BottomNavigation } from '~/sections/common/components/BottomNavigation'
 import { MacroEvolution } from '~/sections/profile/components/MacroEvolution'
 import { MacroProfileComp } from '~/sections/profile/components/MacroProfile'
@@ -5,10 +7,27 @@ import { UserInfo } from '~/sections/profile/components/UserInfo'
 import { MeasuresEvolution } from '~/sections/profile/measure/components/MeasuresEvolution'
 import { WeightEvolution } from '~/sections/weight/components/WeightEvolution'
 
+function useAspectWidth() {
+  const [width, setWidth] = createSignal(getWidth())
+  function getWidth() {
+    return Math.min((window.innerHeight * 9) / 16, window.innerWidth)
+  }
+  function onResize() {
+    setWidth(getWidth())
+  }
+  window.addEventListener('resize', onResize)
+  onCleanup(() => window.removeEventListener('resize', onResize))
+  return width
+}
+
 export default function Page() {
+  const width = useAspectWidth()
   return (
     <div>
-      <div class={'mx-1 md:mx-40 lg:mx-auto lg:w-1/3 pt-1'}>
+      <div
+        class={'mx-auto w-full flex flex-col justify-between pt-1'}
+        style={{ width: `${width()}px` }}
+      >
         <UserInfo />
         <WeightEvolution />
         <MacroProfileComp />
