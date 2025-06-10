30c06ba745776bed6cb5ba28a9a253384ae67283
marcuscastelo <marcuscastelo@usp.br>
2025-06-10 06:20:40 -0300

    refactor(meal): standardize error handling and user feedback in application layer

--- src/modules/diet/meal/application/meal.ts
+++ src/modules/diet/meal/application/meal.ts
@@ -12,31 +12,40 @@ import { handleApiError } from '~/shared/error/errorHandler'
 
 // TODO:   Maybe replace empty arrays with loading state (null or something)
 export const dayMeals = () => currentDayDiet()?.meals ?? []
+// TODO:   Remove dayId from functions that don't need it
 
+/**
+ * Updates a meal in the current day diet.
+ * @param _dayId - The day diet ID (unused).
+ * @param mealId - The meal ID.
+ * @param newMeal - The new meal data.
+ * @returns True if updated, false otherwise.
+ */
 export async function updateMeal(
-  _dayId: DayDiet['id'], // TODO:   Remove dayId from functions that don't need it
+  _dayId: DayDiet['id'],
   mealId: Meal['id'],
   newMeal: Meal,
-) {
+): Promise<boolean> {
   try {
     const currentDayDiet_ = currentDayDiet()
     if (currentDayDiet_ === null) {
-      throw new Error('[meal::application] Current day diet is null')
+      handleApiError('[meal::application] Current day diet is null', {
+        component: 'mealApplication',
+        operation: 'updateMeal',
+        additionalData: { mealId, mealName: newMeal.name },
+      })
+      return false
     }
-
-    // Update meal in day diet
     const updatedDayDiet = updateMealInDayDiet(currentDayDiet_, mealId, newMeal)
-
-    // Convert to NewDayDiet
     const newDay = convertToNewDayDiet(updatedDayDiet)
-
     await updateDayDiet(currentDayDiet_.id, newDay)
+    return true
   } catch (error) {
     handleApiError(error, {
       component: 'mealApplication',
       operation: 'updateMeal',
       additionalData: { mealId, mealName: newMeal.name },
     })
-    throw error
+    return false
   }
 }
