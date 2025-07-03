72eb08b0d20e904ca4b69a229c28ec423672e2df
marcuscastelo <marcuscastelo@usp.br>
2025-07-03 18:52:00 -0300

    refactor(modal-ui): remove GlobalModalContainer in favor of UnifiedModalContainer

--- src/sections/common/context/Providers.tsx
+++ src/sections/common/context/Providers.tsx
@@ -2,10 +2,6 @@ import { type JSXElement } from 'solid-js'
 
 import { lazyImport } from '~/shared/solid/lazyImport'
 
-const { GlobalModalContainer } = lazyImport(
-  () => import('~/modules/toast/ui/GlobalModalContainer'),
-  ['GlobalModalContainer'],
-)
 const { UnifiedModalContainer } = lazyImport(
   () => import('~/shared/modal/components/UnifiedModalContainer'),
   ['UnifiedModalContainer'],
@@ -20,7 +16,6 @@ export function Providers(props: { children: JSXElement }) {
   return (
     <>
       <DarkToaster />
-      <GlobalModalContainer />
       <UnifiedModalContainer />
       {props.children}
     </>
