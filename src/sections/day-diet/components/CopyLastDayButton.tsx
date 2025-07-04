9403390877b1840796a9633485c55242f4bceb04
marcuscastelo <marcuscastelo@usp.br>
2025-07-03 21:17:28 -0300

    refactor(day-diet-ui): remove size property from CopyLastDayModal configuration

--- src/sections/day-diet/components/CopyLastDayButton.tsx
+++ src/sections/day-diet/components/CopyLastDayButton.tsx
@@ -80,7 +80,6 @@ export function CopyLastDayButton(props: {
             ),
             {
               title: 'Copiar dia anterior',
-              size: 'large',
             },
           )
         }}
